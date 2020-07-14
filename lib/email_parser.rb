157c2103ab068d2aca4007fd6e6cc2a0c55ec4b6
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-14 06:50:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,4 @@
-class EmailParser 
+class EmailAddressParser 
   attr_accessor :name, :csv_emails
   
   def initialize(csv_emails)
@@ -14,9 +14,9 @@ class EmailParser
 end 
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
+parser1 = EmailAddressParserParser.new(emails1)
 parser1.parse 
 
 emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
+parser2 = EmailAddressParser.new(emails2)
 parser2.parse
