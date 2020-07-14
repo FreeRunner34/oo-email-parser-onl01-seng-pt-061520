cd9718d8ba73bb09a7b2887762e8457f85a55e8a
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-14 06:30:27 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,6 +1,6 @@
 # Build a class EmailAddressParser that accepts a string of unformatted 
 class EmailAddressParser
-  attr_accessor :csv_emails
+  attr_accessor :name, :csv_emails
 end 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
