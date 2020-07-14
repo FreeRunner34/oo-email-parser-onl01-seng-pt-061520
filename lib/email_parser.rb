353557a6c210026c19e38e0102827f4c710b80ee
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-14 06:47:46 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,5 +1,5 @@
 # Build a class EmailAddressParser that accepts a string of unformatted 
-class EmailAddressParser
+class EmailParser
   attr_accessor :name, :csv_emails
   
   def initialize (csv_emails)
